20d30980f1299f1780cb0232039536473d8fcb0a
lippiece <github@lippiece.anonaddy.me>
2024-03-26 20:01:00 +0200

    feat(client): add alpine and other goodies to astro

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import alpinejs from "@astrojs/alpinejs"
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: "/inventory",
+  integrations: [alpinejs({ entrypoint: "./alpine" })],
+})
